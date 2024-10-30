7982966165fe5839fb51ff006add0e4c4cdfc050
e5571531 <e5571531@host.docker.internal>
2024-10-30 17:14:18 +0530

    naming-server, implemented with eureka

---
+++ naming-server/src/test/java/org/msoct/pompei/eureka/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msoct.pompei.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
